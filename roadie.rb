42ff54405ed4154c5e7ada80f2d17f6e892dc5a0
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2016-08-12 22:37:04 -0400

    Version 0.2.0.

--- roadie.rb
+++ roadie.rb
@@ -1,14 +1,23 @@
+#
+# assets/formula.go
+#
+# Copyright (c) 2016 Junpei Kawamoto
+#
+# This software is released under the MIT License.
+#
+# http://opensource.org/licenses/mit-license.php
+#
 class Roadie < Formula
   desc "A easy way to run your programs on Google Cloud Platform."
   homepage "https://github.com/jkawamoto/roadie"
-  version "0.1.4"
+  version "0.2.0"
 
-  if Hardware.is_64_bit?
-    url "https://github.com/jkawamoto/roadie/releases/download/v0.1.4/roadie_0.1.4_darwin_amd64.zip"
-    sha256 "0974337b85240e4803694a0d7f8cbca4da8e6b6efc8e8855be3ecefb1a763a07"
+  if Hardware::CPU.is_64_bit?
+    url "https://github.com/jkawamoto/roadie/releases/download/v0.2.0/roadie_0.2.0_darwin_amd64.zip"
+    sha256 "1585f1e8dbff1cf4b0cba0ab4d29fbbd22c4f85959df279997aaf12c3140ab3d"
   else
-    url "https://github.com/jkawamoto/roadie/releases/download/v0.1.4/roadie_0.1.4_darwin_386.zip"
-    sha256 "7fbae4e94e1401f6c690eabba3c38e02eee03e791339b166f3d9d670bb8bf08a"
+    url "https://github.com/jkawamoto/roadie/releases/download/v0.2.0/roadie_0.2.0_darwin_386.zip"
+    sha256 "ba23fb3e9ef9a9944c12e78ebbf0360db92d7607106fd972d772a6f4dd135925"
   end
 
   def install
@@ -18,4 +27,5 @@ class Roadie < Formula
   test do
     system "roadie"
   end
+
 end
