d482f22d5430107915d1cd4204e96d970af47139
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2017-02-11 21:21:41 -0500

    Version 0.3.4.

--- roadie.rb
+++ roadie.rb
@@ -1,7 +1,7 @@
 #
 # assets/formula.go
 #
-# Copyright (c) 2016 Junpei Kawamoto
+# Copyright (c) 2016-2017 Junpei Kawamoto
 #
 # This software is released under the MIT License.
 #
@@ -10,17 +10,20 @@
 class Roadie < Formula
   desc "A easy way to run your programs on Google Cloud Platform."
   homepage "https://github.com/jkawamoto/roadie"
-  version "0.3.3"
+  version "0.3.4"
 
   if Hardware::CPU.is_64_bit?
-    url "https://github.com/jkawamoto/roadie/releases/download/v0.3.3/roadie_0.3.3_darwin_amd64.zip"
-    sha256 "c374494ee694b0f53206acc3ebe50d2b402afdbfbe5a771447af32e772cad205"
+    url "https://github.com/jkawamoto/roadie/releases/download/v0.3.4/roadie_0.3.4_darwin_amd64.zip"
+    sha256 "85fa6713ab1638d40f91b657be177aba57e631b50d572b28e65e303581f38a3b"
   else
-    url "https://github.com/jkawamoto/roadie/releases/download/v0.3.3/roadie_0.3.3_darwin_386.zip"
-    sha256 "221bf6d26c1d2a7472d1aa1902fd63a252d01274fd94d1e1b28c72832bac130c"
+    url "https://github.com/jkawamoto/roadie/releases/download/v0.3.4/roadie_0.3.4_darwin_386.zip"
+    sha256 "d934279bc26c4128baff329f97ed21cfdba9d1c9a84839981c7553f3c9b9707f"
   end
 
+  system "brew tap caskroom/cask"
+
   def install
+    system "brew cask install google-cloud-sdk" => :run
     bin.install "roadie"
   end
 
