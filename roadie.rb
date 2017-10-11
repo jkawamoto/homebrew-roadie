e35e4b9d55c338ccec94d7700fb6cecb4f39b617
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2017-10-10 21:32:09 -0400

    Version 0.4.0.

--- roadie.rb
+++ roadie.rb
@@ -20,18 +20,18 @@ require 'rbconfig'
 class Roadie < Formula
   desc "A easy way to run your programs on Google Cloud Platform."
   homepage "https://github.com/jkawamoto/roadie"
-  version "0.3.13"
+  version "0.4.0"
 
   if Hardware::CPU.is_64_bit?
     case RbConfig::CONFIG['host_os']
     when /mswin|msys|mingw|cygwin|bccwin|wince|emc/
       :windows
     when /darwin|mac os/
-      url "https://github.com/jkawamoto/roadie/releases/download/v0.3.13/roadie_0.3.13_darwin_amd64.zip"
-      sha256 "867fb542ec3a4f0c1cb27f48dd6bf6bde7b0790f1a2a5860915558dd8b429c34"
+      url "https://github.com/jkawamoto/roadie/releases/download/v0.4.0/roadie_0.4.0_darwin_amd64.zip"
+      sha256 "4945e12f2b4a2c33f5fb66439b0711cac24fbcd25ca2d7a78e41d11e25151a40"
     when /linux/
-      url "https://github.com/jkawamoto/roadie/releases/download/v0.3.13/roadie_0.3.13_linux_amd64.tar.gz"
-      sha256 "9812257ccb253096f3cba4ad5c72a3894f8fc0cafb29cebe128363ff93b104d4"
+      url "https://github.com/jkawamoto/roadie/releases/download/v0.4.0/roadie_0.4.0_linux_amd64.tar.gz"
+      sha256 "c6ae094046e509ef6ddd4eac363bcd31c33b23ee84e72fd3b5e44638b30cbdc7"
     when /solaris|bsd/
       :unix
     else
@@ -42,11 +42,11 @@ class Roadie < Formula
     when /mswin|msys|mingw|cygwin|bccwin|wince|emc/
       :windows
     when /darwin|mac os/
-      url "https://github.com/jkawamoto/roadie/releases/download/v0.3.13/roadie_0.3.13_darwin_386.zip"
-      sha256 "d00087073f8af769ad1b7ac7fd1630eddf1a69a0f89e184184e0927676523f42"
+      url "https://github.com/jkawamoto/roadie/releases/download/v0.4.0/roadie_0.4.0_darwin_386.zip"
+      sha256 "0806c6080cbd5bc306b2e5594eb88c3f4d8b22c6385d63c940fc259ca334b722"
     when /linux/
-      url "https://github.com/jkawamoto/roadie/releases/download/v0.3.13/roadie_0.3.13_linux_386.tar.gz"
-      sha256 "7bdeb4e2da8c502f2dac2ed2d3c9a3230107e962d5265fbb0adeb5bc7041b3a8"
+      url "https://github.com/jkawamoto/roadie/releases/download/v0.4.0/roadie_0.4.0_linux_386.tar.gz"
+      sha256 "45b41647a37a7bee546203f68a777e0235d0c240356d344eb279c12c0a8869d9"
     when /solaris|bsd/
       :unix
     else
